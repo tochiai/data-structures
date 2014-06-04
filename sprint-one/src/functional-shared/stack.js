fb28f792949ee01ce86aba5ef4f59fd71fd9d0a9
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-04 15:10:44 -0700

    implemented functional shared stack and functional queue

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.sizeVal = 0;
+  return _.extend(someInstance, stackMethods);
 };
 
 var stackMethods = {};
-
+stackMethods.size = function() {
+  return this.sizeVal;
+};
+stackMethods.push = function(value) {
+  this.storage[this.sizeVal] = value;
+  this.sizeVal++;
+};
+stackMethods.pop = function() {
+  if(this.sizeVal > 0) {
+    this.sizeVal--;
+  }
+  var result = this.storage[this.sizeVal];
+  delete this.storage[this.sizeVal];
+  return result;
+};
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var end = 0;
+  var start = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function(){
+    if(someInstance.size() > 0) {
+      var result = storage[start];
+      delete storage[start];
+      start++;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return end-start;
   };
 
   return someInstance;
