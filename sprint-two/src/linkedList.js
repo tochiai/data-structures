bfc98353b3a118113e6fd3a562c39380b95bbc6e
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-05 10:49:43 -0700

    Implmenet linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(list.tail !== null) {
+      list.tail.next = node;
+    }
+    if(list.head === null) {
+      list.head = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var result = list.head;
+    if(list.head !== null) {
+      list.head = list.head.next;
+    }
+    return result.value;
   };
 
   list.contains = function(target){
+    var walkList = function(node) {
+      if(node === null) {
+        return false;
+      } else if(node.value === target) {
+        return true;
+      } else {
+        return walkList(node.next);
+      }
+    };
+    return walkList(list.head);
   };
 
   return list;
