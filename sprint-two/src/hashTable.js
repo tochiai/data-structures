f0a29c1a0d05a549b03d0fa99efbd5b9761dead8
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-05 16:16:10 -0700

    Implemented hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,22 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || {};
+  bucket[k] = v;
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  if (this._storage.get(i) === undefined){
+    return null;
+  } else {
+    return this._storage.get(i)[k];
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  this.insert(k, null);
 };
 
 
